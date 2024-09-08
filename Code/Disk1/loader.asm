134763b1b5b9aa2f2290dba1e774cfc08d1508aa
Kaleidio <christhefennec@gmail.com>
2024-09-08 15:27:43 +0100

    fix loader
    ~fixed several issues with syntax causing wrong values in $0314
    ~respond to vic interrupt to prevent looping deadlock

--- Code/Disk1/loader.asm
+++ Code/Disk1/loader.asm
@@ -7,41 +7,6 @@
   .BORDER_FRAME_TIMER = $02
   .BORDER_COLOR = $03
 
-  !macro LOAD_FILE addrlo, addrhi, namelen, name
-
-    lda #namelen
-    ldx #<name
-    ldy #>name
-    jsr KERNAL.SETNAM
-    
-    lda #15
-    ldx #8
-    ldy #0
-    jsr KERNAL.SETLFS
-    
-    lda #0
-    ldx #addrlo
-    lda #addrhi
-    jsr KERNAL.LOAD
-
-  !end
-
-  !macro RESET_BORDER_TIMER
-    
-    lda #0
-    sta $02
-    
-  !end
-  
-  !macro CHANGE_BORDER_COLOR
-    
-    ;we don't need to worry about the top 4 bits as the VIC ignores them anyway
-    inc $03
-    lda $03
-    sta VIC.BORDER_COLOR
-    
-  !end
-
   * = $0801
   
   ;sys call
@@ -56,8 +21,20 @@
   * = $080D
   
   ;disable basic
-  lda #36
+  lda #$36
   sta $01
+  
+  ;link chain to irq using self modifying code
+  lda $0314
+  sta CHAIN + 1
+  lda $0315
+  sta CHAIN + 2
+  
+  ;then inject own routine
+  lda #<BORDER_COLOR_IRQ
+  sta $0314
+  lda #>BORDER_COLOR_IRQ
+  sta $0315
   
   ;setup raster line irq
   lda #%10000001
@@ -67,31 +44,31 @@
   lda #0 ;we wanna wait for line zero
   sta VIC.RASTER_POS
   
-  ;reset frame counter
-  +RESET_BORDER_TIMER
-  
-  ;setup border colour incremental.  this lets us give an immediate response that the program has started running
-  +CHANGE_BORDER_COLOR
-  
-  ;link chain to irq using self modifying code
-  lda $0314
-  sta CHAIN + 1
-  lda $0315
-  sta CHAIN + 2
-  
-  ;then inject own routine
-  lda <BORDER_COLOR_IRQ
-  sta $0314
-  lda >BORDER_COLOR_IRQ
-  sta $0315
-  
   ;and start loading CORE,PRG
-  +LOAD_FILE $00, $18, CORE_FILE_NAME_END - CORE_FILE_NAME, CORE_FILE_NAME
+  lda #CORE_FILE_NAME_END - CORE_FILE_NAME
+  ldx #<CORE_FILE_NAME
+  ldy #>CORE_FILE_NAME
+  jsr KERNAL.SETNAM
+  
+  lda #15
+  ldx #8
+  ldy #0
+  jsr KERNAL.SETLFS
+  
+  ;TODO: ldx and ldy to position on a code table of CORE.
+  lda #0
+  ldx #$00
+  ldy #$18
+  jsr KERNAL.LOAD
   
   ;once done, jump to the first instruction in the core, exiting this loader.
   jmp $2000
   
   BORDER_COLOR_IRQ:
+    ;respond to vic
+    lda #%00000000
+    sta VIC.IRQ_MASK
+  
     ;count frames
     inc .BORDER_FRAME_TIMER
     lda #150 ;about 3 seconds
@@ -99,10 +76,22 @@
     bne CHAIN ;don't do anything if not enough frames elapsed.
     
     ;increment colour and reset timer.
-    +CHANGE_BORDER_COLOR
-    +RESET_BORDER_TIMER
+    jsr NEXT_COLOR
   
-    CHAIN: jmp $0000
+  CHAIN: 
+    jmp $0000
+    
+  NEXT_COLOR:
+    ;reset frame counter
+    lda #0
+    sta $02
+    
+    ;we don't need to worry about the top 4 bits as the VIC ignores them anyway
+    inc $03
+    lda $03
+    sta VIC.BORDER_COLOR
+    
+    rts
   
   CORE_FILE_NAME:  
     !text "CORE,PRG"
